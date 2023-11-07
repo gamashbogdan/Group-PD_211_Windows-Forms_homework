cd30f477475aaa11d5e2d48ae9a7cb1c10b155e0
gamashbogdan <gamashbogdan@gmail.com>
2023-11-07 18:14:35 +0200

    Add project files.

---
+++ Group-PD_211_Windows-Forms_homework/Form1.cs
@@ -0,0 +1,10 @@
+namespace Group_PD_211_Windows_Forms_homework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
