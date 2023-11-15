4127258ac8d5336d28c7b68f537ef3ba248f1ed6
gamashbogdan <gamashbogdan@gmail.com>
2023-11-15 23:42:08 +0200

    7 - Зробив дз на 20.11

--- 03_Range_Controls_Indicators/Form1.Designer.cs
+++ 03_Range_Controls_Indicators/Form1.Designer.cs
@@ -29,17 +29,26 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             MaxSymbolsLabel = new Label();
             TextBox = new TextBox();
-            SymbolsProgressBar = new ProgressBar();
-            SymbolsLabel = new Label();
-            MaxLenghtLabel = new Label();
             MaxSymbolsNumericUpDown = new NumericUpDown();
             ToolTipProgresBar = new ToolTip(components);
             ToolTipNumericUpDown = new ToolTip(components);
-            CountWordLabel = new Label();
-            WordLabel = new Label();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            toolStripSymbolsProgressBar = new ToolStripProgressBar();
+            toolStripMaxLenghtLabel = new ToolStripStatusLabel();
+            toolStripStatusLabel2 = new ToolStripStatusLabel();
+            toolStripCountNumbersLabel = new ToolStripStatusLabel();
+            toolStripStatusLabel3 = new ToolStripStatusLabel();
+            toolStripCountSymbolsLabel = new ToolStripStatusLabel();
+            toolStripStatusLabel4 = new ToolStripStatusLabel();
+            toolStripCountWordsLabel = new ToolStripStatusLabel();
+            toolStripSetingBatton = new ToolStripSplitButton();
+            setingColorToolStripMenuItem = new ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)MaxSymbolsNumericUpDown).BeginInit();
+            statusStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // MaxSymbolsLabel
@@ -55,43 +64,15 @@
             // TextBox
             // 
             TextBox.BackColor = Color.LightCyan;
-            TextBox.Location = new Point(23, 40);
+            TextBox.Font = new Font("Ink Free", 11.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            TextBox.Location = new Point(12, 40);
             TextBox.MaxLength = 100;
             TextBox.Multiline = true;
             TextBox.Name = "TextBox";
             TextBox.ScrollBars = ScrollBars.Vertical;
-            TextBox.Size = new Size(765, 363);
+            TextBox.Size = new Size(776, 359);
             TextBox.TabIndex = 3;
             TextBox.TextChanged += TextBox_TextChanged;
-            // 
-            // SymbolsProgressBar
-            // 
-            SymbolsProgressBar.ForeColor = SystemColors.ControlText;
-            SymbolsProgressBar.Location = new Point(112, 417);
-            SymbolsProgressBar.Name = "SymbolsProgressBar";
-            SymbolsProgressBar.Size = new Size(140, 17);
-            SymbolsProgressBar.TabIndex = 4;
-            ToolTipProgresBar.SetToolTip(SymbolsProgressBar, "Показує кількість введених слів у вигляді шкали прогресу");
-            // 
-            // SymbolsLabel
-            // 
-            SymbolsLabel.AutoSize = true;
-            SymbolsLabel.Font = new Font("Ink Free", 11.999999F, FontStyle.Bold, GraphicsUnit.Point);
-            SymbolsLabel.Location = new Point(23, 412);
-            SymbolsLabel.Name = "SymbolsLabel";
-            SymbolsLabel.Size = new Size(93, 25);
-            SymbolsLabel.TabIndex = 5;
-            SymbolsLabel.Text = "Symbols ";
-            // 
-            // MaxLenghtLabel
-            // 
-            MaxLenghtLabel.AutoSize = true;
-            MaxLenghtLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            MaxLenghtLabel.Location = new Point(269, 409);
-            MaxLenghtLabel.Name = "MaxLenghtLabel";
-            MaxLenghtLabel.Size = new Size(30, 28);
-            MaxLenghtLabel.TabIndex = 6;
-            MaxLenghtLabel.Text = ":D";
             // 
             // MaxSymbolsNumericUpDown
             // 
@@ -118,25 +99,90 @@
             ToolTipNumericUpDown.Tag = "";
             ToolTipNumericUpDown.ToolTipIcon = ToolTipIcon.Info;
             // 
-            // CountWordLabel
+            // statusStrip1
             // 
-            CountWordLabel.AutoSize = true;
-            CountWordLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            CountWordLabel.Location = new Point(721, 406);
-            CountWordLabel.Name = "CountWordLabel";
-            CountWordLabel.Size = new Size(30, 28);
-            CountWordLabel.TabIndex = 8;
-            CountWordLabel.Text = ";D";
+            statusStrip1.ImageScalingSize = new Size(20, 20);
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, toolStripSymbolsProgressBar, toolStripMaxLenghtLabel, toolStripStatusLabel2, toolStripCountNumbersLabel, toolStripStatusLabel3, toolStripCountSymbolsLabel, toolStripStatusLabel4, toolStripCountWordsLabel, toolStripSetingBatton });
+            statusStrip1.Location = new Point(0, 417);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 33);
+            statusStrip1.TabIndex = 10;
+            statusStrip1.Text = "statusStrip1";
             // 
-            // WordLabel
+            // toolStripStatusLabel1
             // 
-            WordLabel.AutoSize = true;
-            WordLabel.Font = new Font("Ink Free", 13.1999989F, FontStyle.Bold, GraphicsUnit.Point);
-            WordLabel.Location = new Point(658, 409);
-            WordLabel.Name = "WordLabel";
-            WordLabel.Size = new Size(60, 27);
-            WordLabel.TabIndex = 9;
-            WordLabel.Text = "Word";
+            toolStripStatusLabel1.Font = new Font("Ink Free", 13.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(90, 27);
+            toolStripStatusLabel1.Text = "Symbols";
+            // 
+            // toolStripSymbolsProgressBar
+            // 
+            toolStripSymbolsProgressBar.Name = "toolStripSymbolsProgressBar";
+            toolStripSymbolsProgressBar.Size = new Size(140, 25);
+            // 
+            // toolStripMaxLenghtLabel
+            // 
+            toolStripMaxLenghtLabel.Name = "toolStripMaxLenghtLabel";
+            toolStripMaxLenghtLabel.Size = new Size(47, 27);
+            toolStripMaxLenghtLabel.Text = "0/100";
+            // 
+            // toolStripStatusLabel2
+            // 
+            toolStripStatusLabel2.Font = new Font("Ink Free", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            toolStripStatusLabel2.Size = new Size(73, 27);
+            toolStripStatusLabel2.Text = "Number";
+            // 
+            // toolStripCountNumbersLabel
+            // 
+            toolStripCountNumbersLabel.Name = "toolStripCountNumbersLabel";
+            toolStripCountNumbersLabel.Size = new Size(17, 27);
+            toolStripCountNumbersLabel.Text = "0";
+            toolStripCountNumbersLabel.TextDirection = ToolStripTextDirection.Horizontal;
+            // 
+            // toolStripStatusLabel3
+            // 
+            toolStripStatusLabel3.Font = new Font("Ink Free", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            toolStripStatusLabel3.Size = new Size(64, 27);
+            toolStripStatusLabel3.Text = "Symbol";
+            // 
+            // toolStripCountSymbolsLabel
+            // 
+            toolStripCountSymbolsLabel.Name = "toolStripCountSymbolsLabel";
+            toolStripCountSymbolsLabel.Size = new Size(17, 27);
+            toolStripCountSymbolsLabel.Text = "0";
+            // 
+            // toolStripStatusLabel4
+            // 
+            toolStripStatusLabel4.Font = new Font("Ink Free", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            toolStripStatusLabel4.Size = new Size(48, 27);
+            toolStripStatusLabel4.Text = "Word";
+            // 
+            // toolStripCountWordsLabel
+            // 
+            toolStripCountWordsLabel.Name = "toolStripCountWordsLabel";
+            toolStripCountWordsLabel.Size = new Size(17, 27);
+            toolStripCountWordsLabel.Text = "0";
+            // 
+            // toolStripSetingBatton
+            // 
+            toolStripSetingBatton.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripSetingBatton.DropDownItems.AddRange(new ToolStripItem[] { setingColorToolStripMenuItem });
+            toolStripSetingBatton.Image = (Image)resources.GetObject("toolStripSetingBatton.Image");
+            toolStripSetingBatton.ImageTransparentColor = Color.Magenta;
+            toolStripSetingBatton.Name = "toolStripSetingBatton";
+            toolStripSetingBatton.Size = new Size(39, 31);
+            toolStripSetingBatton.Text = "toolStripSplitButton1";
+            toolStripSetingBatton.ButtonClick += toolStripSetingBatton_ButtonClick;
+            // 
+            // setingColorToolStripMenuItem
+            // 
+            setingColorToolStripMenuItem.Name = "setingColorToolStripMenuItem";
+            setingColorToolStripMenuItem.Size = new Size(224, 26);
+            setingColorToolStripMenuItem.Text = "Seting color";
             // 
             // Form1
             // 
@@ -144,17 +190,17 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(800, 450);
-            Controls.Add(WordLabel);
-            Controls.Add(CountWordLabel);
+            Controls.Add(statusStrip1);
             Controls.Add(MaxSymbolsNumericUpDown);
-            Controls.Add(MaxLenghtLabel);
-            Controls.Add(SymbolsLabel);
-            Controls.Add(SymbolsProgressBar);
             Controls.Add(TextBox);
             Controls.Add(MaxSymbolsLabel);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
-            Text = "Form1";
+            Text = "Notepad--";
+            FormClosing += Form1_FormClosing;
             ((System.ComponentModel.ISupportInitialize)MaxSymbolsNumericUpDown).EndInit();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -163,13 +209,20 @@
 
         private Label MaxSymbolsLabel;
         private TextBox TextBox;
-        private ProgressBar SymbolsProgressBar;
-        private Label SymbolsLabel;
-        private Label MaxLenghtLabel;
         private NumericUpDown MaxSymbolsNumericUpDown;
         private ToolTip ToolTipProgresBar;
         private ToolTip ToolTipNumericUpDown;
-        private Label CountWordLabel;
-        private Label WordLabel;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private ToolStripProgressBar toolStripSymbolsProgressBar;
+        private ToolStripStatusLabel toolStripMaxLenghtLabel;
+        private ToolStripStatusLabel toolStripCountNumbersLabel;
+        private ToolStripStatusLabel toolStripCountSymbolsLabel;
+        private ToolStripStatusLabel toolStripCountWordsLabel;
+        private ToolStripStatusLabel toolStripStatusLabel2;
+        private ToolStripStatusLabel toolStripStatusLabel3;
+        private ToolStripStatusLabel toolStripStatusLabel4;
+        private ToolStripSplitButton toolStripSetingBatton;
+        private ToolStripMenuItem setingColorToolStripMenuItem;
     }
 }
--- 03_Range_Controls_Indicators/Form1.cs
+++ 03_Range_Controls_Indicators/Form1.cs
@@ -1,30 +1,109 @@
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
 namespace Dz_na_20._11
 {
     public partial class Form1 : Form
     {
+        static string saveWords = null;
         public Form1()
         {
             InitializeComponent();
-            MaxLenghtLabel.Text = $"{this.Opacity}/{TextBox.MaxLength}";
-            CountWordLabel.Text = $"0";
+            string loadeWords = LoadFromFile();
+            if (loadeWords.Length != 0)
+            {
+                if (MessageBox.Show("Load from file?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    MaxSymbolsNumericUpDown.Value = loadeWords.Length;
+
+                    toolStripSymbolsProgressBar.Maximum = (int)MaxSymbolsNumericUpDown.Value;
+                    toolStripMaxLenghtLabel.Text = $"{loadeWords.Length}/{TextBox.MaxLength}";
+                    string[] words = TextBox.Text.Split(new char[] { ' ', ',', '.', '-', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                    int wordCount = words.Length;
+                    string pattern = @"\d+";
+                    MatchCollection res = Regex.Matches(TextBox.Text, pattern);
+                    toolStripCountNumbersLabel.Text = $"{res.Count}              ";
+                    int countSymbols = 0;
+                    foreach (char symbols in TextBox.Text)
+                    {
+                        if (Char.IsLetter(symbols))
+                            countSymbols++;
+                    }
+                    toolStripCountSymbolsLabel.Text = $"{countSymbols}              ";
+                    toolStripCountWordsLabel.Text = $"{wordCount}              ";
+                    TextBox.Text = loadeWords;
+                }
+
+            }
+        }
+        static void SaveToFile()
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(string));
+            try
+            {
+                using (Stream fs = File.Create("Words.xml"))
+                {
+                    formatter.Serialize(fs, saveWords);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static string LoadFromFile()
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(string));
+            string loadeWords = null;
+            using (Stream fs = File.OpenRead("Words.xml"))
+            {
+                if (fs.Length > 0)
+                {
+                    loadeWords = (string)formatter.Deserialize(fs);
+                }
+            }
+            return loadeWords;
         }
         private void MaxSymbolsNumericUpDown_ValueChanged(object sender, EventArgs e)
         {
             TextBox.MaxLength = (int)MaxSymbolsNumericUpDown.Value;
-            MaxLenghtLabel.Text = $"{this.Opacity}/{TextBox.MaxLength}";
-            SymbolsProgressBar.Maximum = (int)MaxSymbolsNumericUpDown.Value;
+            toolStripMaxLenghtLabel.Text = $"{this.Opacity}/{TextBox.MaxLength}";
+            toolStripSymbolsProgressBar.Maximum = (int)MaxSymbolsNumericUpDown.Value;
         }
         private void TextBox_TextChanged(object sender, EventArgs e)
         {
             TextBox.MaxLength = (int)MaxSymbolsNumericUpDown.Value;
-            MaxLenghtLabel.Text = $"{TextBox.Text.Length}/{TextBox.MaxLength}";
-            SymbolsProgressBar.Maximum = (int)MaxSymbolsNumericUpDown.Value;
-            SymbolsProgressBar.Value = TextBox.Text.Length;
+            toolStripMaxLenghtLabel.Text = $"{TextBox.Text.Length}/{TextBox.MaxLength}";
+            toolStripSymbolsProgressBar.Maximum = (int)MaxSymbolsNumericUpDown.Value;
+            toolStripSymbolsProgressBar.Value = TextBox.Text.Length;
             string[] words = TextBox.Text.Split(new char[] { ' ', ',', '.', '-', '\n' }, StringSplitOptions.RemoveEmptyEntries);
             int wordCount = words.Length;
-            CountWordLabel.Text = $"{wordCount}";
+            string pattern = @"\d+";
+            MatchCollection res = Regex.Matches(TextBox.Text, pattern);
+            toolStripCountNumbersLabel.Text = $"{res.Count}              ";
+            int countSymbols = 0;
+            foreach (char symbols in TextBox.Text)
+            {
+                if (Char.IsLetter(symbols))
+                    countSymbols++;
+            }
+            toolStripCountSymbolsLabel.Text = $"{countSymbols}              ";
+            toolStripCountWordsLabel.Text = $"{wordCount}              ";
 
         }
 
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            saveWords = TextBox.Text;
+            if (saveWords.Length != 0)
+            {
+                if (MessageBox.Show("Save Text?", "", MessageBoxButtons.YesNoCancel) == DialogResult.Yes)
+                {
+                    SaveToFile();
+                }
+            }
+        }
+        private void toolStripSetingBatton_ButtonClick(object sender, EventArgs e)
+        {
+        }
     }
 }
